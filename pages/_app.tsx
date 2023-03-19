5c9f5e470d4f5ab5891f122856949227b475effd
Hartaithan <hartaithan@gmail.com>
2023-03-19 09:51:24 +0900

    project clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = (props: AppProps) => {
+  const { Component, pageProps } = props;
+  return <Component {...pageProps} />;
+};
+
+export default App;
