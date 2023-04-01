610a7614a21eefc6fd7e9b7be5e3d9623996899f
Hartaithan <hartaithan@gmail.com>
2023-04-01 12:14:37 +0900

    add sign out api route

---
+++ pages/api/signOut.ts
@@ -0,0 +1,30 @@
+import { deleteCookie } from "cookies-next";
+import { type NextApiHandler } from "next";
+
+const signOut: NextApiHandler = async (req, res) => {
+  const options = { req, res };
+
+  try {
+    deleteCookie("psn-access-token", options);
+    deleteCookie("psn-refresh-token", options);
+    deleteCookie("supabase-auth-token", options);
+    return res.status(200).json({ message: "Successful sign out!" });
+  } catch (error) {
+    return res.status(400).json({ message: "Unable to sign out" });
+  }
+};
+
+const handler: NextApiHandler = async (req, res) => {
+  const { method } = req;
+  switch (method) {
+    case "GET":
+      return signOut(req, res);
+    default:
+      res.setHeader("Allow", ["GET"]);
+      return res
+        .status(405)
+        .end(`Method ${method ?? "[Not Found]"} Not Allowed`);
+  }
+};
+
+export default handler;
