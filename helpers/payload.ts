2f2fe6a9b95e8764e4415faed85d627e5b7ab02c
Hartaithan <hartaithan@gmail.com>
2023-04-08 17:03:11 +0900

    add payload validation helper

---
+++ helpers/payload.ts
@@ -0,0 +1,35 @@
+type IPayloadObject = Record<string, unknown>;
+
+interface IValidationResult {
+  message: string;
+  errors: string[];
+}
+
+export const validatePayload = (
+  payload: IPayloadObject,
+  ignore: string[] = []
+): IValidationResult | null => {
+  const results: string[] = [];
+  const items = Object.entries(payload);
+  if (items.length === 0) {
+    return { message: "Empty payload", errors: [] };
+  }
+  for (let i = 0; i < items.length; i++) {
+    const [key, value] = items[i];
+    const allowedKey = ignore.includes(key);
+    let check = value === undefined;
+    if (typeof value === "string") {
+      check = value.length === 0;
+    }
+    if (allowedKey) {
+      continue;
+    }
+    if (check) {
+      results.push(key + " is required");
+    }
+  }
+  if (results.length > 0) {
+    return { message: "Invalid payload", errors: results };
+  }
+  return null;
+};
