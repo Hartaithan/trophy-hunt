5a3b1523a905f5cc217417710fa17c2c8642711d
Hartaithan <hartaithan@gmail.com>
2023-04-13 13:43:16 +0900

    display debug data on main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import { type IPage } from "@/models/AppModel";
 import { useProfile } from "@/providers/PSNProvider";
-import { Flex, Title, Text, Button } from "@mantine/core";
-import { useUser } from "@supabase/auth-helpers-react";
+import { Flex, Title, Text, Button, Group } from "@mantine/core";
+import { useSession, useUser } from "@supabase/auth-helpers-react";
 import Link from "next/link";
 
 const Home: IPage = () => {
   const user = useUser();
+  const session = useSession();
   const { profile, updateProfile } = useProfile();
+
   return (
     <Flex h="100%" justify="center" align="center" direction="column" gap={12}>
       <Title>Hello world!</Title>
@@ -14,14 +16,32 @@ const Home: IPage = () => {
       <Link href="/board" prefetch={false}>
         to /board
       </Link>
-      <Flex>
-        <Text component="pre" size={10}>
-          {JSON.stringify(user, null, 2)}
+      <Group align="flex-start">
+        <Text
+          component="pre"
+          w={400}
+          size={10}
+          style={{ whiteSpace: "break-spaces", wordBreak: "break-all" }}
+        >
+          user: {JSON.stringify(user, null, 2)}
         </Text>
-        <Text component="pre" size={10}>
-          {JSON.stringify(profile, null, 2)}
+        <Text
+          component="pre"
+          w={400}
+          size={10}
+          style={{ whiteSpace: "break-spaces", wordBreak: "break-all" }}
+        >
+          session: {JSON.stringify(session, null, 2)}
         </Text>
-      </Flex>
+        <Text
+          component="pre"
+          w={400}
+          size={10}
+          style={{ whiteSpace: "break-spaces", wordBreak: "break-all" }}
+        >
+          psn_profile: {JSON.stringify(profile, null, 2)}
+        </Text>
+      </Group>
     </Flex>
   );
 };
