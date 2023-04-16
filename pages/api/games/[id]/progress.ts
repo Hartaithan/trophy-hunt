0e36c319b75f8d02bf81498fc5d856cdcad066af
Hartaithan <hartaithan@gmail.com>
2023-04-16 10:27:10 +0900

    merge new progress with previous

--- pages/api/games/[id]/progress.ts
+++ pages/api/games/[id]/progress.ts
@@ -1,6 +1,33 @@
-import { type IProgressPayload } from "@/models/GameModel";
-import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { type NextApiHandler } from "next";
+import { type IProgressItem, type IProgressPayload } from "@/models/GameModel";
+import {
+  type SupabaseClient,
+  createServerSupabaseClient,
+} from "@supabase/auth-helpers-nextjs";
+import { type NextApiResponse, type NextApiHandler } from "next";
+
+const updateProgress = async (
+  id: string,
+  progress: IProgressItem[],
+  supabase: SupabaseClient,
+  res: NextApiResponse
+): Promise<unknown> => {
+  const { data: updateData, error: updateError } = await supabase
+    .from("games")
+    .update({ progress })
+    .eq("id", id)
+    .select("progress")
+    .single();
+  if (updateError !== null) {
+    console.error("unable to update game progress", id, updateError);
+    return res
+      .status(400)
+      .json({ message: "Unable to update game progress", id });
+  }
+  return res.status(200).json({
+    message: "Game progress successfully updated!",
+    data: updateData.progress,
+  });
+};
 
 const saveGameProgress: NextApiHandler = async (req, res) => {
   const {
@@ -9,6 +36,10 @@ const saveGameProgress: NextApiHandler = async (req, res) => {
   } = req;
   const { payload } = body as IProgressPayload;
   const supabase = createServerSupabaseClient({ req, res });
+
+  if (id === undefined || Array.isArray(id)) {
+    return res.status(400).json({ message: "Invalid [id] query" });
+  }
 
   if (payload == null || !Array.isArray(payload) || payload.length === 0) {
     return res.status(400).json({ message: "Invalid payload" });
@@ -26,29 +57,17 @@ const saveGameProgress: NextApiHandler = async (req, res) => {
   }
 
   if (data.progress === null) {
-    const { data: updateData, error: updateError } = await supabase
-      .from("games")
-      .update({ progress: payload })
-      .eq("id", id)
-      .select("progress")
-      .single();
-    if (updateError !== null) {
-      console.error("unable to update game progress", id, error);
-      return res
-        .status(400)
-        .json({ message: "Unable to update game progress", id });
-    }
-    return res.status(200).json({
-      message: "Game progress successfully updated!",
-      data: updateData.progress,
-    });
+    return await updateProgress(id, payload, supabase, res);
   }
 
-  return res.status(200).json({
-    message: "Hello world!",
-    old: data.progress,
-    new: payload,
-  });
+  const previous = [...data.progress];
+  const updated = [...payload];
+  const merged = updated.map((i) => ({
+    ...previous.find((n) => n.id === i.id),
+    ...i,
+  }));
+
+  return await updateProgress(id, merged, supabase, res);
 };
 
 const handler: NextApiHandler = async (req, res) => {
