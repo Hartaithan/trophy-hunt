a7bb0ccb098a5660d7d18f1aa1f4bff516d1d272
Hartaithan <hartaithan@gmail.com>
2023-04-29 19:37:50 +0900

    custom stable scrollbar

--- styles/global.ts
+++ styles/global.ts
@@ -4,6 +4,17 @@ export const globalStyles = (theme: MantineTheme): CSSObject => ({
   "*": {
     color: theme.colors.secondary[8],
   },
+  "::-webkit-scrollbar": {
+    width: 6,
+    background: theme.colors.primary[8],
+  },
+  "::-webkit-scrollbar-thumb": {
+    borderRadius: 5,
+    background: theme.colors.primary[5],
+  },
+  "::-webkit-scrollbar-thumb:hover": {
+    background: theme.colors.primary[4],
+  },
   body: {
     ...theme.fn.fontStyles(),
     backgroundColor: theme.colors.primary[8],
