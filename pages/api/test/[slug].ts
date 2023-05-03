c89774b0187e881afb0f32b488940d16fb0bc311
Hartaithan <hartaithan@gmail.com>
2023-05-03 13:25:56 +0900

    delete test routes

--- pages/api/test/[slug].ts
+++
@@ -1,7 +0,0 @@
-import { type NextApiHandler } from "next";
-
-const handler: NextApiHandler = async (req, res) => {
-  return res.status(200).json({ data: req.query });
-};
-
-export default handler;
