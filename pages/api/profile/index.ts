d29acaae84115b75238869b51bd5d6ee0720cc64
Hartaithan <hartaithan@gmail.com>
2023-05-15 13:34:41 +0900

    add update profile route

--- pages/api/profile/index.ts
+++ pages/api/profile/index.ts
@@ -1,3 +1,4 @@
+import { validatePayload } from "@/helpers/payload";
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { type NextApiHandler } from "next";
 
@@ -27,7 +28,39 @@ const getProfile: NextApiHandler = async (req, res) => {
 };
 
 const updateProfile: NextApiHandler = async (req, res) => {
-  return res.status(200).json({ message: "PUT: Hello World!" });
+  const { body } = req;
+  const supabase = createServerSupabaseClient({ req, res });
+
+  const results = validatePayload(body);
+  if (results !== null) {
+    console.error("invalid payload", results.errors);
+    return res.status(400).json(results);
+  }
+
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+  if (userError !== null || user === null) {
+    console.error("unable to get user", userError);
+    return res.status(400).json({ message: "Unable to get user" });
+  }
+
+  const { data: profile, error } = await supabase
+    .from("profiles")
+    .update(body)
+    .eq("id", user.id)
+    .select("*")
+    .single();
+
+  if (error !== null) {
+    console.error("unable to update profile", error);
+    return res.status(400).json({ message: "Unable to update profile" });
+  }
+
+  return res
+    .status(200)
+    .json({ message: "Profile successfully updated!", profile });
 };
 
 const handler: NextApiHandler = async (req, res) => {
