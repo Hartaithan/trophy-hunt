fac723a2a079f90e19c62df9f2d02510d66c9d06
Hartaithan <hartaithan@gmail.com>
2023-05-21 12:22:22 +0900

    move group in separate component

---
+++ components/TrophyGroup.tsx
@@ -0,0 +1,66 @@
+import { type IGroup } from "@/models/TrophyModel";
+import { Text, Flex, createStyles } from "@mantine/core";
+import Image from "next/image";
+import { type FC } from "react";
+
+interface ITrophyGroupProps {
+  group: IGroup;
+}
+
+const useStyles = createStyles(({ spacing }) => ({
+  container: {
+    marginBottom: spacing.md,
+  },
+  icon: { height: "auto", objectFit: "contain" },
+  info: {
+    marginLeft: spacing.md,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+}));
+
+const TrophyGroup: FC<ITrophyGroupProps> = (props) => {
+  const { group } = props;
+  const { classes } = useStyles();
+
+  const { icon_url, name, counts } = group;
+
+  return (
+    <Flex className={classes.container}>
+      <Image
+        width={150}
+        height={100}
+        className={classes.icon}
+        src={icon_url}
+        alt={name ?? "group icon url"}
+      />
+      <Flex className={classes.info}>
+        <Text fw="bold">{name}</Text>
+        <Text>
+          {counts.platinum > 0 && (
+            <Text span inherit>
+              {`Platinum: ${counts.platinum}, `}
+            </Text>
+          )}
+          {counts.gold > 0 && (
+            <Text span inherit>
+              {`Gold: ${counts.gold}, `}
+            </Text>
+          )}
+          {counts.silver > 0 && (
+            <Text span inherit>
+              {`Silver: ${counts.silver}, `}
+            </Text>
+          )}
+          {counts.bronze > 0 && (
+            <Text span inherit>
+              {`Bronze: ${counts.bronze}`}
+            </Text>
+          )}
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default TrophyGroup;
--- components/TrophyList.tsx
+++ components/TrophyList.tsx
@@ -1,23 +1,15 @@
 import { type IFormattedResponse } from "@/models/TrophyModel";
 import { type FC } from "react";
 import TrophyCard from "./TrophyCard";
-import { Box, Flex, Text, createStyles, Stack } from "@mantine/core";
-import Image from "next/image";
+import { Box, createStyles, Stack } from "@mantine/core";
+import TrophyGroup from "./TrophyGroup";
 
 interface ITrophyListProps {
   trophies: IFormattedResponse | null;
 }
 
-const useStyles = createStyles(({ spacing }) => ({
-  group: {
-    marginBottom: spacing.md,
-  },
-  groupIcon: { height: "auto", objectFit: "contain" },
-  groupInfo: {
-    marginLeft: spacing.md,
-    flexDirection: "column",
-    justifyContent: "center",
-  },
+const useStyles = createStyles(() => ({
+  container: {},
 }));
 
 const TrophyList: FC<ITrophyListProps> = (props) => {
@@ -27,43 +19,10 @@ const TrophyList: FC<ITrophyListProps> = (props) => {
   if (trophies === null) return null;
 
   return (
-    <div>
+    <div className={classes.container}>
       {trophies.groups.map((group) => (
         <Box key={group.id}>
-          <Flex className={classes.group}>
-            <Image
-              width={150}
-              height={100}
-              className={classes.groupIcon}
-              src={group.icon_url}
-              alt={group.name ?? "group icon url"}
-            />
-            <Flex className={classes.groupInfo}>
-              <Text fw="bold">{group.name}</Text>
-              <Text>
-                {group.counts.platinum > 0 && (
-                  <Text span inherit>
-                    {`Platinum: ${group.counts.platinum}, `}
-                  </Text>
-                )}
-                {group.counts.gold > 0 && (
-                  <Text span inherit>
-                    {`Gold: ${group.counts.gold}, `}
-                  </Text>
-                )}
-                {group.counts.silver > 0 && (
-                  <Text span inherit>
-                    {`Silver: ${group.counts.silver}, `}
-                  </Text>
-                )}
-                {group.counts.bronze > 0 && (
-                  <Text span inherit>
-                    {`Bronze: ${group.counts.bronze}`}
-                  </Text>
-                )}
-              </Text>
-            </Flex>
-          </Flex>
+          <TrophyGroup group={group} />
           <Stack>
             {group.trophies.map((trophy) => (
               <TrophyCard key={trophy.id} trophy={trophy} />
