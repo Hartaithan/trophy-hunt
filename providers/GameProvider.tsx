9509342d0fb6d916bfc79e597b75fae3d831aed7
Hartaithan <hartaithan@gmail.com>
2023-05-25 13:08:48 +0900

    add game provider

---
+++ providers/GameProvider.tsx
@@ -0,0 +1,74 @@
+import API from "@/helpers/api";
+import { type IGame } from "@/models/GameModel";
+import { type IFormattedResponse } from "@/models/TrophyModel";
+import {
+  type PropsWithChildren,
+  type FC,
+  createContext,
+  useState,
+  useContext,
+} from "react";
+
+interface IGameProviderProps extends PropsWithChildren {
+  id: string;
+  initialGame?: IGame | null;
+  initialTrophies?: IFormattedResponse | null;
+}
+
+interface IGameContext {
+  game: IGame | null;
+  trophies: IFormattedResponse | null;
+  refetchGame: () => void;
+  refetchTrophies: () => void;
+}
+
+const initialContextValue: IGameContext = {
+  game: null,
+  trophies: null,
+  refetchGame: () => null,
+  refetchTrophies: () => null,
+};
+
+const Context = createContext<IGameContext>(initialContextValue);
+
+const GameProvider: FC<IGameProviderProps> = (props) => {
+  const { children, id, initialGame = null, initialTrophies = null } = props;
+
+  const [game, setGame] = useState<IGame | null>(initialGame);
+  const [trophies, setTrophies] = useState<IFormattedResponse | null>(
+    initialTrophies
+  );
+
+  const refetchGame = (): void => {
+    API.get("/games/" + id)
+      .then(({ data }) => {
+        setGame(data?.game ?? null);
+      })
+      .catch((error) => {
+        console.error("unable to refetch game", error);
+      });
+  };
+
+  const refetchTrophies = (): void => {
+    API.get("/games/" + id + "/earned")
+      .then(({ data }) => {
+        setTrophies(data ?? null);
+      })
+      .catch((error) => {
+        console.error("unable to refetch trophies", error);
+      });
+  };
+
+  const exposed: IGameContext = {
+    game,
+    trophies,
+    refetchGame,
+    refetchTrophies,
+  };
+
+  return <Context.Provider value={exposed}>{children}</Context.Provider>;
+};
+
+export const useGame = (): IGameContext => useContext(Context);
+
+export default GameProvider;
