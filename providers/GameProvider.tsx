59d4876da36a9e5245209d85562182a9d9d36203
Hartaithan <hartaithan@gmail.com>
2023-05-27 16:18:17 +0900

    sync progress on backend

--- providers/GameProvider.tsx
+++ providers/GameProvider.tsx
@@ -2,12 +2,16 @@ import API from "@/helpers/api";
 import { type IGame } from "@/models/GameModel";
 import { type IProgressItem } from "@/models/ProgressModel";
 import { type IFormattedResponse } from "@/models/TrophyModel";
+import { useDebouncedValue } from "@mantine/hooks";
+import { notifications } from "@mantine/notifications";
 import {
   type PropsWithChildren,
   type FC,
   createContext,
   useState,
   useContext,
+  useEffect,
+  useRef,
 } from "react";
 
 interface IGameProviderProps extends PropsWithChildren {
@@ -49,6 +53,8 @@ const GameProvider: FC<IGameProviderProps> = (props) => {
   const [trophies, setTrophies] = useState<IFormattedResponse | null>(
     initialTrophies
   );
+  const [debouncedProgress] = useDebouncedValue(progress, 700);
+  const isUserInteract = useRef<boolean>(false);
 
   const refetchGame = (): void => {
     if (typeof id !== "string") return;
@@ -82,6 +88,7 @@ const GameProvider: FC<IGameProviderProps> = (props) => {
   };
 
   const updateProgress = (id: number): void => {
+    isUserInteract.current = true;
     setProgress((prev) => {
       const updated = [...prev].map((i) => {
         return { ...i, earned: i.id === id ? !i.earned : i.earned };
@@ -99,6 +106,33 @@ const GameProvider: FC<IGameProviderProps> = (props) => {
     refetchTrophies,
     updateProgress,
   };
+
+  useEffect(() => {
+    if (typeof id !== "string") return;
+    if (!isUserInteract.current) return;
+    setLoading(true);
+    const payload = { payload: debouncedProgress };
+    API.post("/games/" + id + "/progress", JSON.stringify(payload))
+      .then(({ data }) => {
+        notifications.show({
+          title: "Success!",
+          message: data.message,
+          autoClose: 3000,
+        });
+      })
+      .catch((error) => {
+        console.error("unable to sync trophies progress", error);
+        notifications.show({
+          title: "Something went wrong!",
+          color: "red",
+          message: error.response.data.message,
+          autoClose: false,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [debouncedProgress]); // eslint-disable-line
 
   return <Context.Provider value={exposed}>{children}</Context.Provider>;
 };
