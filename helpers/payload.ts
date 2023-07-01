5305d0e2690dbed86a285dbdfccef911b8b07d8e
Hartaithan <hartaithan@gmail.com>
2023-07-01 13:05:49 +0900

    add required fields check in validate payload

--- helpers/payload.ts
+++ helpers/payload.ts
@@ -1,17 +1,41 @@
-type IPayloadObject = Record<string, unknown>;
+type PayloadItem = [string, unknown];
+type PayloadObject = Record<string, unknown>;
 
 interface IValidationResult {
   message: string;
   errors: string[];
 }
 
-export const validatePayload = (
-  payload: IPayloadObject,
-  ignore: string[] = []
-): IValidationResult | null => {
+interface IRequiredResult {
+  hasAllFields: boolean;
+  errors: string[];
+}
+
+interface IEmptyResult {
+  results: string[];
+}
+
+export const checkRequiredFields = (
+  items: PayloadItem[],
+  fields: string[]
+): IRequiredResult => {
   const results: string[] = [];
-  const items = Object.entries(payload);
-  if (items.length === 0) return { message: "Empty payload", errors: [] };
+  let hasAllFields = true;
+  for (let n = 0; n < fields.length; n++) {
+    const key = fields[n];
+    const keyIsPassed = items.some((i) => i[0] === key);
+    if (keyIsPassed) continue;
+    results.push(key + " is required");
+    hasAllFields = false;
+  }
+  return { hasAllFields, errors: results };
+};
+
+const checkEmptyFields = (
+  items: PayloadItem[],
+  ignore: string[]
+): IEmptyResult => {
+  const results: string[] = [];
   for (let i = 0; i < items.length; i++) {
     const [key, value] = items[i];
     const allowedKey = ignore.includes(key);
@@ -21,9 +45,26 @@ export const validatePayload = (
     }
     if (allowedKey) continue;
     if (check) {
-      results.push(key + " is required");
+      results.push(key + " is empty");
     }
   }
+  return { results };
+};
+
+export const validatePayload = (
+  payload: PayloadObject,
+  required: string[] | null = null,
+  ignore: string[] = []
+): IValidationResult | null => {
+  const items = Object.entries(payload);
+  if (required != null) {
+    const { hasAllFields, errors } = checkRequiredFields(items, required);
+    if (!hasAllFields) {
+      return { message: "Invalid payload", errors };
+    }
+  }
+  if (items.length === 0) return { message: "Empty payload", errors: [] };
+  const { results } = checkEmptyFields(items, ignore);
   if (results.length === 0) return null;
   return { message: "Invalid payload", errors: results };
 };
