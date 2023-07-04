dc5e8fdd8e8286506fac263102651dbeac13e20a
Hartaithan <hartaithan@gmail.com>
2023-07-04 13:29:27 +0900

    add get note by id route

--- pages/api/notes/[id]/index.ts
+++ pages/api/notes/[id]/index.ts
@@ -1,7 +1,29 @@
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { type NextApiHandler } from "next";
 
 const getNote: NextApiHandler = async (req, res) => {
-  return res.status(200).json({ message: "Hello World!" });
+  const {
+    query: { id },
+  } = req;
+  const supabase = createServerSupabaseClient({ req, res });
+
+  if (id === undefined || Array.isArray(id)) {
+    console.error("invalid [id] query", req.query);
+    return res.status(400).json({ message: "Invalid [id] query" });
+  }
+
+  const { data, error } = await supabase
+    .from("notes")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error !== null) {
+    console.error("unable to get note by id", id, error);
+    return res.status(400).json({ message: "Unable to get note by id", id });
+  }
+
+  return res.status(200).json({ note: data });
 };
 
 const updateNote: NextApiHandler = async (req, res) => {
