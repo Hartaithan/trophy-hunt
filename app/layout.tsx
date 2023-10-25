483e4ee523640868c361699ef5fb15dfbe5e185a
Hartaithan <hartaithan@gmail.com>
2023-10-25 11:45:41 +0900

    install eslint config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { FC, PropsWithChildren } from "react";
+import { type FC, type PropsWithChildren } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
