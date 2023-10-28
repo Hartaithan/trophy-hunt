d1607399366f120570b1e4a59550bad8ea5f4b1d
Hartaithan <hartaithan@gmail.com>
2023-10-28 20:03:22 +0900

    add auth middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import { Button, Title } from "@mantine/core";
+import { Title } from "@mantine/core";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { type NextPage } from "next";
+import { cookies } from "next/headers";
 
-const Home: NextPage = () => {
+const Home: NextPage = async () => {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const { data } = await supabase.auth.getSession();
+
   return (
     <main>
       <Title>Hello World!</Title>
-      <Button>Click</Button>
+      <pre style={{ maxWidth: 400, overflow: "auto", fontSize: 8 }}>
+        {JSON.stringify(data, null, 2)}
+      </pre>
     </main>
   );
 };
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,72 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse, type NextMiddleware } from "next/server";
+import { type NullableAuthResponse } from "./models/AuthModel";
+import { exchangeRefreshTokenForAuthTokens } from "psn-api";
+import { cookies } from "next/headers";
+
+export const config = {
+  matcher: "/((?!api|static|.*\\..*|_next|favicon.ico).*)",
+};
+
+const refreshTokens = async (token: string): Promise<NullableAuthResponse> => {
+  let authorization: NullableAuthResponse = null;
+  try {
+    authorization = await exchangeRefreshTokenForAuthTokens(token);
+  } catch (error) {
+    console.error("unable to refresh tokens", error);
+  }
+  return authorization;
+};
 
 export const middleware: NextMiddleware = async (req) => {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
-  await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  let access_token = req.cookies.get("psn-access-token")?.value;
+  let refresh_token = req.cookies.get("psn-refresh-token")?.value;
+
+  const isAuthPage = ["/signIn", "/signUp"].includes(req.nextUrl.pathname);
+
+  let refreshed_auth: NullableAuthResponse = null;
+  const redirectUrl = req.nextUrl.clone();
+
+  if (access_token === undefined && refresh_token !== undefined) {
+    refreshed_auth = await refreshTokens(refresh_token);
+    if (refreshed_auth != null) {
+      const { accessToken, refreshToken } = refreshed_auth;
+      access_token = accessToken;
+      refresh_token = refreshToken;
+    }
+  }
+
+  const isAuth =
+    access_token !== undefined &&
+    refresh_token !== undefined &&
+    session != null;
+
+  if (!isAuth && !isAuthPage) {
+    const allCookies = cookies().getAll();
+    redirectUrl.pathname = "/signIn";
+    const redirectRes = NextResponse.redirect(redirectUrl);
+    for (let i = 0; i < allCookies.length; i++) {
+      const cookie = allCookies[i];
+      redirectRes.cookies.delete(cookie.name);
+    }
+    return redirectRes;
+  }
+
+  if (isAuth && isAuthPage) {
+    redirectUrl.pathname = "/";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (refreshed_auth != null) {
+    const { accessToken, expiresIn } = refreshed_auth;
+    res.cookies.set("psn-access-token", accessToken, { maxAge: expiresIn });
+  }
+
   return res;
 };
