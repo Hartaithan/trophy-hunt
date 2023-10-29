95b2f9298e5fdb2e99009f44e73f3df6991fad1b
Hartaithan <hartaithan@gmail.com>
2023-10-29 14:20:33 +0900

    add refresh token route

---
+++ app/api/auth/refresh/route.ts
@@ -0,0 +1,58 @@
+import { type NullableAuthResponse } from "@/models/AuthModel";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { exchangeRefreshTokenForAuthTokens } from "psn-api";
+
+export const GET = async (): Promise<Response> => {
+  const cookieStore = cookies();
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
+
+  const access_token = cookies().get("psn-access-token");
+  const refresh_token = cookies().get("psn-refresh-token");
+
+  if (access_token !== undefined) {
+    console.error("user already has psn-access-token", access_token);
+    return Response.json(
+      { message: "You already have an access token" },
+      { status: 400 },
+    );
+  }
+
+  if (typeof refresh_token !== "string") {
+    console.error("psn-refresh-token not found", refresh_token);
+    return Response.json(
+      { message: "Unable to get refresh token" },
+      { status: 400 },
+    );
+  }
+
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+  if (userError !== null || user === null) {
+    console.error("unable to get user", userError);
+    return Response.json({ message: "Unable to get user" }, { status: 400 });
+  }
+
+  let authorization: NullableAuthResponse = null;
+  try {
+    authorization = await exchangeRefreshTokenForAuthTokens(refresh_token);
+  } catch (error) {
+    console.error("unable to refresh tokens", error);
+  }
+
+  if (authorization == null) {
+    return Response.json(
+      { message: "Unable to refresh tokens" },
+      { status: 400 },
+    );
+  }
+
+  const { accessToken, expiresIn } = authorization;
+  cookies().set("psn-access-token", accessToken, {
+    maxAge: expiresIn,
+  });
+
+  return Response.json({ message: "Tokens successfully refreshed!" });
+};
