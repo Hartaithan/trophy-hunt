a8c1f16092f9a8c0d630400537c093a08fdfc94e
Hartaithan <hartaithan@gmail.com>
2023-10-31 12:32:13 +0900

    add image domains in config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.playstation.net",
+      },
+      {
+        protocol: "http",
+        hostname: "**.playstation.net",
+      },
+      {
+        protocol: "https",
+        hostname: "**.playstation.com",
+      },
+      {
+        protocol: "http",
+        hostname: "**.playstation.com",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
