e67905d590fee20fe99b4b7284627105e92ecb6e
Hartaithan <hartaithan@gmail.com>
2023-11-19 17:58:23 +0900

    validate session before sign out

--- app/api/auth/signOut/route.ts
+++ app/api/auth/signOut/route.ts
@@ -1,12 +1,26 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 export const GET = async (): Promise<Response> => {
   try {
+    const supabase = createRouteHandlerClient({ cookies });
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session == null) {
+      return Response.json(
+        { message: "You've already sign out!" },
+        { status: 400 },
+      );
+    }
+
     const allCookies = cookies().getAll();
     for (let i = 0; i < allCookies.length; i++) {
       const cookie = allCookies[i];
       cookies().delete(cookie.name);
     }
+
     return Response.json({ message: "Successful sign out!" });
   } catch (error) {
     console.error("unable to sign out", error);
