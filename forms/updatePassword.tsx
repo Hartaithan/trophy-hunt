3ebc78f1fad62d64f984a7741207d6d79c9345f7
Hartaithan <hartaithan@gmail.com>
2023-11-20 12:11:19 +0900

    confirm password validation

--- forms/updatePassword.tsx
+++ forms/updatePassword.tsx
@@ -1,29 +1,51 @@
 "use client";
 
+import { type UpdatePasswordBody } from "@/models/AuthModel";
 import { Box, Button, Flex, Grid, PasswordInput } from "@mantine/core";
+import { hasLength, useForm } from "@mantine/form";
 import { type FC } from "react";
 
 const UpdatePasswordForm: FC = () => {
+  const form = useForm<UpdatePasswordBody>({
+    initialValues: {
+      current_password: "",
+      new_password: "",
+    },
+    validate: {
+      current_password: hasLength(
+        { min: 6 },
+        "Password should include at least 6 characters",
+      ),
+      new_password: (value, values) =>
+        value !== values.current_password ? "Passwords did not match" : null,
+    },
+    validateInputOnChange: true,
+  });
+
+  const handleSubmit = (values: typeof form.values): void => {
+    console.info("values", values);
+  };
+
   return (
-    <Box>
+    <Box component="form" onSubmit={form.onSubmit(handleSubmit)}>
       <Grid>
         <Grid.Col span={6}>
           <PasswordInput
-            id="current_password"
-            label="Password"
-            placeholder="Password"
+            label="Current Password"
+            placeholder="Current Password"
+            {...form.getInputProps("current_password")}
           />
         </Grid.Col>
         <Grid.Col span={6}>
           <PasswordInput
-            id="new_password"
-            label="Confirm password"
-            placeholder="Confirm password"
+            label="New password"
+            placeholder="New password"
+            {...form.getInputProps("new_password")}
           />
         </Grid.Col>
       </Grid>
       <Flex justify="flex-end">
-        <Button type="submit" w={150} mt="xl">
+        <Button type="submit" w={150} mt="xl" disabled={!form.isValid()}>
           Update!
         </Button>
       </Flex>
