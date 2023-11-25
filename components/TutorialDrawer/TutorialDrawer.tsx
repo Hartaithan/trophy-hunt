37a3b4072b702b9057aa7d7b01304bfb7d656426
Hartaithan <hartaithan@gmail.com>
2023-11-25 18:27:02 +0900

    add tutorial content layout

--- components/TutorialDrawer/TutorialDrawer.tsx
+++ components/TutorialDrawer/TutorialDrawer.tsx
@@ -1,15 +1,117 @@
 "use client";
 
-import { Drawer, type DrawerProps } from "@mantine/core";
+import {
+  Anchor,
+  Drawer,
+  Stack,
+  type DrawerProps,
+  Stepper,
+  Flex,
+  Alert,
+} from "@mantine/core";
 import { memo, type FC } from "react";
+import classes from "./TutorialDrawer.module.css";
+import { Text } from "@mantine/core";
+import {
+  IconAlertOctagon,
+  IconChecks,
+  IconTableShare,
+  IconUserCheck,
+} from "@tabler/icons-react";
+import { theme } from "@/styles/theme";
 
 type Props = DrawerProps;
 
 const TutorialDrawer: FC<Props> = (props) => {
   const { ...rest } = props;
   return (
-    <Drawer title="Tutorial" position="right" {...rest}>
-      Hello World!
+    <Drawer
+      classNames={classes}
+      title="How to get NPSSO?"
+      position="right"
+      size="lg"
+      {...rest}>
+      <Stack className={classes.explanation}>
+        <Text size="sm">
+          So, uh, okay. You probably don&apos;t understand what the fuck the
+          NPSSO is, do you? I&apos;ll explain.
+        </Text>
+        <Text size="sm">
+          PlayStation has a <b>private API</b>, and I can&apos;t just grab your
+          trophy data whenever I want. So I have to access the PSN API with an
+          authorization token, which is what <b>NPSSO</b> is. And the only way
+          to get it is to authorize on the PSN Store website.
+        </Text>
+        <Text size="sm">
+          And if you think it&apos;s a scam, okay, I feel you. But look at&nbsp;
+          <Anchor
+            href="https://www.reddit.com/r/Trophies/comments/phqhmu/other_is_there_a_way_to_get_access_token_to_use"
+            target="_blank">
+            this
+          </Anchor>
+          &nbsp;and
+          <Anchor
+            href="https://psn-api.achievements.app/authentication/authenticating-manually"
+            target="_blank">
+            &nbsp;this
+          </Anchor>
+          &nbsp;discussion. It&apos;s literally, <b>the only way</b> to
+          communicate with the PSN API. There&apos;s nothing I can do about it.
+          I wish it was more user friendly too, but alas, it&apos;s not possible
+          at the moment.
+        </Text>
+      </Stack>
+      <Flex className={classes.steps}>
+        <Text id="heading" size="lg" fw={500} ta="center">
+          So, how do you get the NPSSO?
+        </Text>
+        <Stepper active={-1} orientation="vertical">
+          <Stepper.Step
+            icon={<IconUserCheck />}
+            label="Step 1"
+            description={
+              <Text size="sm">
+                Sign in on the&nbsp;
+                <Anchor href="https://store.playstation.com" target="_blank">
+                  PSN Store
+                </Anchor>
+              </Text>
+            }
+          />
+          <Stepper.Step
+            icon={<IconTableShare />}
+            label="Step 2"
+            description={
+              <Text size="sm">
+                Visit&nbsp;
+                <Anchor
+                  href="https://ca.account.sony.com/api/v1/ssocookie"
+                  target="_blank">
+                  this page
+                </Anchor>
+                &nbsp;to see your NPSSO token.
+              </Text>
+            }
+          />
+          <Stepper.Step
+            icon={<IconChecks />}
+            label="Step 3"
+            description="Go back and fill the field"
+          />
+        </Stepper>
+      </Flex>
+      <Alert
+        variant="light"
+        color="red"
+        title="Invalid Grant Error"
+        icon={
+          <IconAlertOctagon
+            color={theme.colors?.red != null ? theme.colors.red[0] : undefined}
+          />
+        }>
+        If you&apos;re seeing an &apos;invalid_grant&apos; error, you failed to
+        log in. Try clearing your cookies and try again
+      </Alert>
     </Drawer>
   );
 };
