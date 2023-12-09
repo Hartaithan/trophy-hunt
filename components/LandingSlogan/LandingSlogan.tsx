dd23284636c74f6ea9486cf9e8a4188e25c09423
Hartaithan <hartaithan@gmail.com>
2023-12-09 18:12:07 +0900

    add gradients to slides

--- components/LandingSlogan/LandingSlogan.tsx
+++ components/LandingSlogan/LandingSlogan.tsx
@@ -2,15 +2,38 @@ import { type FC } from "react";
 import classes from "./LandingSlogan.module.css";
 import { Flex, Text } from "@mantine/core";
 
+const slides = [
+  {
+    title: "Plan",
+    gradient: { from: "#11998e", to: "#38ef7d", deg: 45 },
+  },
+  {
+    title: "Track",
+    gradient: { from: "#0082c8", to: "#667db6", deg: 45 },
+  },
+  {
+    title: "Complete",
+    gradient: { from: "#e53935", to: "#e35d5b", deg: 45 },
+  },
+];
+
 const LandingSlogan: FC = () => {
   return (
     <Flex className={classes.container}>
       <Flex fw={600} className={classes.slider}>
-        <Text>Plan</Text>
-        <Text>Track</Text>
-        <Text>Complete</Text>
+        {slides.map((item) => (
+          <Text key={item.title} variant="gradient" gradient={item.gradient}>
+            {item.title}
+          </Text>
+        ))}
       </Flex>
-      <Text className={classes.caption}>your platinums!</Text>
+      <Text
+        fw={500}
+        className={classes.caption}
+        variant="gradient"
+        gradient={{ from: "#c8d1d8", to: "#eef2f3", deg: 45 }}>
+        your platinums!
+      </Text>
     </Flex>
   );
 };
