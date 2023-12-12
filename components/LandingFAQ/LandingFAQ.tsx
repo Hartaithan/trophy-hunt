97155879706d613e27efcbbe3af164c11e282c79
Hartaithan <hartaithan@gmail.com>
2023-12-12 15:30:57 +0900

    fill faq questions and answers

--- components/LandingFAQ/LandingFAQ.tsx
+++ components/LandingFAQ/LandingFAQ.tsx
@@ -16,30 +16,29 @@ const LandingFAQ: FC = () => {
       </Title>
       <Accordion variant="separated">
         <AccordionItem value="first-question">
-          <AccordionControl>First question</AccordionControl>
+          <AccordionControl>Is it free?</AccordionControl>
           <AccordionPanel>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            quidem, eius enim deserunt tempore mollitia tenetur reprehenderit
-            error veniam. Illo omnis nulla alias ullam non dolor facilis at?
-            Eius, provident.
+            Yes, it&apos;s completely free. What&apos;s more, we don&apos;t have
+            a single ad. Use our service without limitations!
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="second-question">
-          <AccordionControl>Second question</AccordionControl>
+          <AccordionControl>
+            How does the authorization process work?
+          </AccordionControl>
           <AccordionPanel>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            quidem, eius enim deserunt tempore mollitia tenetur reprehenderit
-            error veniam. Illo omnis nulla alias ullam non dolor facilis at?
-            Eius, provident.
+            Our service has synchronization with PSN API, so we ask you to enter
+            NPSSO authorization token to be able to work with it. In addition,
+            we also authorize users in our service, in order to store data about
+            your progress.
           </AccordionPanel>
         </AccordionItem>
         <AccordionItem value="third-question">
-          <AccordionControl>Third question</AccordionControl>
+          <AccordionControl>What is NPSSO?</AccordionControl>
           <AccordionPanel>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            quidem, eius enim deserunt tempore mollitia tenetur reprehenderit
-            error veniam. Illo omnis nulla alias ullam non dolor facilis at?
-            Eius, provident.
+            NPSOO is an authorization token that is needed to work with PSN API.
+            Without it, it would not be possible to synchronize your progress
+            and trophies.
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
