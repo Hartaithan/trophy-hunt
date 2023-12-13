1bde870a8d518a399ce4fb11383660a851dc46db
Hartaithan <hartaithan@gmail.com>
2023-12-13 10:40:49 +0900

    make both sides of slogan animated

--- components/LandingSlogan/LandingSlogan.tsx
+++ components/LandingSlogan/LandingSlogan.tsx
@@ -2,13 +2,17 @@ import { type FC } from "react";
 import classes from "./LandingSlogan.module.css";
 import { Flex, Text } from "@mantine/core";
 import { slogan } from "@/constants/landing";
+import clsx from "clsx";
 
 const LandingSlogan: FC = () => {
   return (
     <Flex className={classes.container}>
-      <Flex fw={600} className={classes.slider}>
+      <Flex fw={600} className={clsx(classes.slider, classes.sliderTitle)}>
         {slogan.map((item) => (
-          <Text key={item.title} variant="gradient" gradient={item.gradient}>
+          <Text
+            key={"title" + item.title}
+            variant="gradient"
+            gradient={item.gradient}>
             {item.title}
           </Text>
         ))}
@@ -18,8 +22,18 @@ const LandingSlogan: FC = () => {
         className={classes.caption}
         variant="gradient"
         gradient={{ from: "#c8d1d8", to: "#eef2f3", deg: 45 }}>
-        your platinums!
+        your
       </Text>
+      <Flex fw={600} className={clsx(classes.slider, classes.sliderTarget)}>
+        {slogan.map((item) => (
+          <Text
+            key={"target" + item.target}
+            variant="gradient"
+            gradient={item.gradient}>
+            {item.target}!
+          </Text>
+        ))}
+      </Flex>
     </Flex>
   );
 };
