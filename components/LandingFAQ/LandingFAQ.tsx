4f8783019312e6bcba4f344267e5c4640c0dcd61
Hartaithan <hartaithan@gmail.com>
2023-12-17 14:07:05 +0900

    add more questions

--- components/LandingFAQ/LandingFAQ.tsx
+++ components/LandingFAQ/LandingFAQ.tsx
@@ -6,6 +6,7 @@ import {
   Box,
   Title,
   Flex,
+  Anchor,
 } from "@mantine/core";
 import { type FC } from "react";
 
@@ -17,14 +18,14 @@ const LandingFAQ: FC = () => {
       </Title>
       <Box w="70%">
         <Accordion variant="separated">
-          <AccordionItem value="first-question">
-            <AccordionControl>Is it free?</AccordionControl>
+          <AccordionItem value="pricing">
+            <AccordionControl>Does it cost anything?</AccordionControl>
             <AccordionPanel>
-              Yes, it is completely free. Moreover, we do not have any ads. Use
+              No, it is completely free. Moreover, we do not have any ads. Use
               our service without any limitations!
             </AccordionPanel>
           </AccordionItem>
-          <AccordionItem value="second-question">
+          <AccordionItem value="authorization">
             <AccordionControl>
               How does the authorization process work?
             </AccordionControl>
@@ -35,12 +36,32 @@ const LandingFAQ: FC = () => {
               about your progress.
             </AccordionPanel>
           </AccordionItem>
-          <AccordionItem value="third-question">
+          <AccordionItem value="npsso">
             <AccordionControl>What is NPSSO?</AccordionControl>
             <AccordionPanel>
               NPSSO is an authorization token that is required to work with PSN
               API. Without it, it would not be possible to synchronize your
               progress and trophies.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem value="profile-type">
+            <AccordionControl>What is a profile type?</AccordionControl>
+            <AccordionPanel>
+              A profile type is a setting that controls the visibility of your
+              board. You can choose between public or private types. A public
+              board can be viewed by anyone, while a private board is only
+              visible to you.
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem value="support">
+            <AccordionControl>
+              How do I contact support if I have problems or questions?
+            </AccordionControl>
+            <AccordionPanel>
+              You can always email me at&nbsp;
+              <Anchor href="mailto:hartaithan@gmail.com?subject=[Trophy Hunt]: Support">
+                hartaithan@gmail.com
+              </Anchor>
             </AccordionPanel>
           </AccordionItem>
         </Accordion>
