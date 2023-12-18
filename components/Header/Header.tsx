604c40e3bb351b8bb43205997034a430b8515fca
Hartaithan <hartaithan@gmail.com>
2023-12-18 14:33:32 +0900

    responsive header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import { type HeaderLink } from "@/models/LinkModel";
-import { type FC } from "react";
+import { useMemo, type FC } from "react";
 import {
   Anchor,
   Badge,
+  Box,
+  Burger,
   Button,
   Container,
   Flex,
   Menu,
+  Paper,
   Title,
+  Transition,
   UnstyledButton,
 } from "@mantine/core";
 import Image from "../Image/Image";
@@ -20,6 +24,7 @@ import { usePathname, useRouter } from "next/navigation";
 import classes from "./Header.module.css";
 import clsx from "clsx";
 import { type NullablePSNProfile } from "@/models/AuthModel";
+import { useDisclosure } from "@mantine/hooks";
 
 interface HeaderProps {
   profile?: NullablePSNProfile;
@@ -27,7 +32,7 @@ interface HeaderProps {
 
 const HEADER_HEIGHT = 48;
 
-const links: HeaderLink[] = [
+const items: HeaderLink[] = [
   {
     id: 1,
     label: "Home",
@@ -46,6 +51,7 @@ const Header: FC<HeaderProps> = (props) => {
   const { profile = null } = props;
   const pathname = usePathname();
   const { refresh } = useRouter();
+  const [opened, { toggle }] = useDisclosure(false);
 
   const handleSignOut = (): void => {
     if (profile == null) return;
@@ -58,73 +64,102 @@ const Header: FC<HeaderProps> = (props) => {
       });
   };
 
+  const links = useMemo(
+    () =>
+      items.map((link) => {
+        const isActive = pathname === link.href;
+        return (
+          <Anchor
+            size="sm"
+            component={Link}
+            key={link.id}
+            href={link.href}
+            className={clsx(
+              classes.link,
+              link.disabled && classes.disabledLink,
+              isActive && classes.activeLink,
+            )}>
+            {link.label}
+          </Anchor>
+        );
+      }),
+    [pathname],
+  );
+
   return (
     <Flex className={classes.root} mih={HEADER_HEIGHT}>
       <Container className={classes.container}>
         <Title order={4}>Trophy Hunt</Title>
-        <Flex className={classes.links}>
-          {links.map((link) => {
-            const isActive = pathname === link.href;
-            return (
-              <Anchor
-                size="sm"
-                component={Link}
-                key={link.id}
-                href={link.href}
-                className={clsx(
-                  classes.link,
-                  link.disabled && classes.disabledLink,
-                  isActive && classes.activeLink,
-                )}>
-                {link.label}
-              </Anchor>
-            );
-          })}
+        <Flex className={classes.links} visibleFrom="xs">
+          {links}
         </Flex>
-        {profile != null ? (
-          <Menu width={150} position="bottom-end">
-            <Menu.Target>
-              <UnstyledButton className={classes.profile}>
-                <Badge mr="sm" radius="sm">
-                  {profile?.onlineId ?? "[Not Found]"}
-                </Badge>
-                <Image
-                  width={30}
-                  height={30}
-                  src={
-                    profile?.avatarUrls?.length > 0
-                      ? profile.avatarUrls[0].avatarUrl
-                      : ""
-                  }
-                  alt="avatar"
-                />
-              </UnstyledButton>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item
-                component={Link}
-                href="/profile"
-                rightSection={<IconUser size="1rem" />}
-                lh="initial">
-                Profile
-              </Menu.Item>
-              <Menu.Item
-                onClick={handleSignOut}
-                rightSection={<IconDoorExit size="1rem" />}>
-                Sign out
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-        ) : (
-          <Button
-            component={Link}
-            href="/signIn"
-            size="compact-xs"
-            leftSection={<IconUser size="0.75rem" />}
-            variant="light">
-            Sign in
-          </Button>
-        )}
+        <Box visibleFrom="xs">
+          {profile != null ? (
+            <Menu width={150} position="bottom-end">
+              <Menu.Target>
+                <UnstyledButton className={classes.profile}>
+                  <Badge mr="sm" radius="sm">
+                    {profile?.onlineId ?? "[Not Found]"}
+                  </Badge>
+                  <Image
+                    width={30}
+                    height={30}
+                    src={
+                      profile?.avatarUrls?.length > 0
+                        ? profile.avatarUrls[0].avatarUrl
+                        : ""
+                    }
+                    alt="avatar"
+                  />
+                </UnstyledButton>
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Item
+                  component={Link}
+                  href="/profile"
+                  rightSection={<IconUser size="1rem" />}
+                  lh="initial">
+                  Profile
+                </Menu.Item>
+                <Menu.Item
+                  onClick={handleSignOut}
+                  rightSection={<IconDoorExit size="1rem" />}>
+                  Sign out
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          ) : (
+            <Button
+              component={Link}
+              href="/signIn"
+              size="compact-xs"
+              leftSection={<IconUser size="0.75rem" />}
+              variant="light">
+              Sign in
+            </Button>
+          )}
+        </Box>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="xs"
+          ml="auto"
+          size="sm"
+        />
+        <Transition transition="slide-down" duration={200} mounted={opened}>
+          {(styles) => (
+            <Paper
+              className={classes.dropdown}
+              hiddenFrom="xs"
+              style={{
+                ...styles,
+                transform: styles.transform + " translateX(-50%)",
+                top: HEADER_HEIGHT + 12,
+              }}>
+              {links}
+            </Paper>
+          )}
+        </Transition>
       </Container>
     </Flex>
   );
