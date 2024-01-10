fdfb3ff342bb6d7f75395527c286decb82bb58b4
Hartaithan <hartaithan@gmail.com>
2024-01-10 14:40:14 +0900

    fix accordion inner elements font-size

--- components/LandingFAQ/LandingFAQ.tsx
+++ components/LandingFAQ/LandingFAQ.tsx
@@ -18,24 +18,19 @@ const LandingFAQ: FC = () => {
         Frequently Asked Questions
       </Title>
       <Box className={classes.wrapper}>
-        <Accordion
-          variant="separated"
-          radius="md"
-          classNames={{ item: classes.accordionItem }}>
+        <Accordion variant="separated" radius="md" classNames={classes}>
           <AccordionItem value="pricing">
-            <AccordionControl className={classes.control}>
-              Does it cost anything?
-            </AccordionControl>
-            <AccordionPanel className={classes.panel}>
+            <AccordionControl>Does it cost anything?</AccordionControl>
+            <AccordionPanel>
               No, it is completely free. Moreover, we do not have any ads. Use
               our service without any limitations!
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem value="authorization">
-            <AccordionControl className={classes.control}>
+            <AccordionControl>
               How does the authorization process work?
             </AccordionControl>
-            <AccordionPanel className={classes.panel}>
+            <AccordionPanel>
               Our service synchronizes with PSN API, so we ask you to enter your
               NPSSO authorization token to be able to work with it. In addition,
               we also authorize users in our service, so that we can store data
@@ -43,20 +38,16 @@ const LandingFAQ: FC = () => {
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem value="npsso">
-            <AccordionControl className={classes.control}>
-              What is NPSSO?
-            </AccordionControl>
-            <AccordionPanel className={classes.panel}>
-              NPSSO is an authorization token that is required to work with PSN
-              API. Without it, it would not be possible to synchronize your
-              progress and trophies.
+            <AccordionControl>What is NPSSO?</AccordionControl>
+            <AccordionPanel>
+              NPSSO is an authorization token required to work with the PSN API.
+              Without it, it is impossible to get trophy data and synchronize
+              your progress.
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem value="profile-type">
-            <AccordionControl className={classes.control}>
-              What is a profile type?
-            </AccordionControl>
-            <AccordionPanel className={classes.panel}>
+            <AccordionControl>What is a profile type?</AccordionControl>
+            <AccordionPanel>
               A profile type is a setting that controls the visibility of your
               board. You can choose between public or private types. A public
               board can be viewed by anyone, while a private board is only
@@ -64,10 +55,10 @@ const LandingFAQ: FC = () => {
             </AccordionPanel>
           </AccordionItem>
           <AccordionItem value="support">
-            <AccordionControl className={classes.control}>
+            <AccordionControl>
               How do I contact support if I have problems or questions?
             </AccordionControl>
-            <AccordionPanel className={classes.panel}>
+            <AccordionPanel>
               You can always email me at&nbsp;
               <Anchor href="mailto:hartaithan@gmail.com?subject=[Trophy Hunt]: Support">
                 hartaithan@gmail.com
