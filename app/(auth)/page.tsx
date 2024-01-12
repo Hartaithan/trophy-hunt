7459ad6ec441ae0f1178ea1d9b99b4e1016b700e
Hartaithan <hartaithan@gmail.com>
2024-01-12 15:34:50 +0900

    fetch recent games in home section

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,23 +1,45 @@
 import HomeSection from "@/components/HomeSection/HomeSection";
 import Landing from "@/components/Landing/Landing";
 import SuccessNotification from "@/components/SuccessNotification/SuccessNotification";
+import { API_URL } from "@/constants/api";
 import { type Page } from "@/models/AppModel";
+import { type Game } from "@/models/GameModel";
+import { getRefreshedCookies } from "@/utils/cookies";
 import { createClient } from "@/utils/supabase/server";
 import { Flex } from "@mantine/core";
+import { type Session } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 
 interface Params {
   success: string | undefined;
 }
 
-const Home: Page<unknown, Params> = async ({ searchParams: { success } }) => {
+interface Response {
+  games: Game[];
+  session: Session | null;
+}
+
+const fetchHomeData = async (): Promise<Response> => {
+  const refreshedCookies = getRefreshedCookies();
   const supabase = createClient(cookies());
-  const { data } = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (session === null) return { games: [], session };
+  const response = await fetch(`${API_URL}/games/recent`, {
+    headers: { Cookie: refreshedCookies },
+  });
+  const { games = [] } = await response.json();
+  return { games, session };
+};
+
+const Home: Page<unknown, Params> = async ({ searchParams: { success } }) => {
+  const data = await fetchHomeData();
 
   return (
     <Flex direction="column" py="xl">
       {data.session != null ? (
-        <HomeSection session={data.session} />
+        <HomeSection games={data.games} session={data.session} />
       ) : (
         <Landing />
       )}
--- components/HomeSection/HomeSection.tsx
+++ components/HomeSection/HomeSection.tsx
@@ -1,25 +1,35 @@
+import { type Game } from "@/models/GameModel";
 import { type Session } from "@supabase/supabase-js";
 import { type FC } from "react";
 
 interface Props {
+  games: Game[];
   session: Session;
 }
 
 const HomeSection: FC<Props> = (props) => {
-  const { session } = props;
+  const { games, session } = props;
   return (
-    <pre
-      style={{
-        maxWidth: 400,
-        overflow: "auto",
-        fontSize: 8,
-        whiteSpace: "pre-wrap",
-      }}>
-      there should be profile stats, last 5 games and etc
-      <br />
-      <br />
-      {JSON.stringify(session, null, 2)}
-    </pre>
+    <div style={{ display: "flex" }}>
+      <pre
+        style={{
+          flex: 1,
+          overflow: "auto",
+          fontSize: 8,
+          whiteSpace: "pre-wrap",
+        }}>
+        {JSON.stringify(games, null, 2)}
+      </pre>
+      <pre
+        style={{
+          flex: 1,
+          overflow: "auto",
+          fontSize: 8,
+          whiteSpace: "pre-wrap",
+        }}>
+        {JSON.stringify(session, null, 2)}
+      </pre>
+    </div>
   );
 };
 
