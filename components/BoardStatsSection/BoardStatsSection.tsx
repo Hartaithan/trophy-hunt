8998225e053588fee2fe4c0c44eb1a3bff8faaa8
Hartaithan <hartaithan@gmail.com>
2024-01-18 14:47:06 +0900

    change board stats section layout

--- components/BoardStatsSection/BoardStatsSection.tsx
+++ components/BoardStatsSection/BoardStatsSection.tsx
@@ -2,9 +2,10 @@
 
 import { columnsFullLabels } from "@/constants/board";
 import { type BoardStats, type BOARD_COLUMNS } from "@/models/BoardModel";
-import { Flex, Grid, RingProgress, Text } from "@mantine/core";
+import { Box, Flex, Grid, RingProgress, Text, Title } from "@mantine/core";
 import { type FC } from "react";
 import classes from "./BoardStatsSection.module.css";
+import { useMediaQuery } from "@mantine/hooks";
 
 interface Props {
   stats: BoardStats | null;
@@ -12,33 +13,38 @@ interface Props {
 
 const BoardStatsSection: FC<Props> = (props) => {
   const { stats } = props;
+  const isMobile = useMediaQuery(`(max-width: 62em)`) ?? false;
 
   if (stats === null) return null;
 
   return (
-    <Flex className={classes.container}>
-      <RingProgress
-        size={150}
-        sections={stats.sections}
-        label={
-          <Text className={classes.ringLabel}>{stats.backlogPercent}%</Text>
-        }
-      />
-      <Grid className={classes.counts}>
-        {Object.entries(stats.counts).map(([key, value]) => (
-          <Grid.Col key={key} span={{ base: 6, md: 3 }}>
-            <Flex className={classes.count}>
-              <Text className={classes.countLabel}>
-                {columnsFullLabels[key as BOARD_COLUMNS]}
-              </Text>
-              <Text key={key} className={classes.countValue}>
-                {value}
-              </Text>
-            </Flex>
-          </Grid.Col>
-        ))}
-      </Grid>
-    </Flex>
+    <Box className={classes.container}>
+      <Title className={classes.title}>Board statistics</Title>
+      <Flex className={classes.content}>
+        <RingProgress
+          size={isMobile ? 200 : 250}
+          thickness={isMobile ? 15 : 25}
+          sections={stats.sections}
+          label={
+            <Text className={classes.ringLabel}>{stats.backlogPercent}%</Text>
+          }
+        />
+        <Grid className={classes.counts}>
+          {Object.entries(stats.counts).map(([key, value]) => (
+            <Grid.Col key={key} span={{ base: 6 }}>
+              <Flex className={classes.count}>
+                <Text className={classes.countLabel}>
+                  {columnsFullLabels[key as BOARD_COLUMNS]}
+                </Text>
+                <Text key={key} className={classes.countValue}>
+                  {value}
+                </Text>
+              </Flex>
+            </Grid.Col>
+          ))}
+        </Grid>
+      </Flex>
+    </Box>
   );
 };
 
