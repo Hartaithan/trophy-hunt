d22b38dd1ddff42250e6483272fedfecc30cbe9a
Hartaithan <hartaithan@gmail.com>
2025-07-27 13:50:12 +0900

    app clean up

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,29 +1,8 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import { FC } from "react";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const RootLayout: FC = () => {
+  return <Stack />;
+};
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+export default RootLayout;
---
+++ app/app/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import { Text, View } from "react-native";
+
+const Index: FC = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default Index;
